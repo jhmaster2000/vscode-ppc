cc2ec8415ec0d3f152a81609fdd8157d389f7c6b
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-04-08 12:51:26 -0300

    Add additional testcases

---
+++ testcases/color-util.cpp
@@ -0,0 +1,12 @@
+/* The extension doesn't actually do anything with CPP files */
+/* This file is just for stealing CPP syntax colors :D       */
+
+class ClassInfo;
+
+class Class {
+    int build(ClassInfo*);
+};
+
+int Class::build(ClassInfo* ThisColorPlease) {
+
+}
